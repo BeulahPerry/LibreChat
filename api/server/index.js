5b48d77c6b561ed02fd94dd0f6dcc9135fd83a9f
BeulahPerry <208416002+BeulahPerry@users.noreply.github.com>
2025-06-11 06:28:30 +0300

    librechat.yaml中的version改成 https://www.librechat.ai/changelog 中最新的

--- api/server/index.js
+++ api/server/index.js
@@ -1,4 +1,4 @@
-require('dotenv').config({ path: '/etc/secrets/.env' });
+require('dotenv').config();
 const path = require('path');
 require('module-alias')({ base: path.resolve(__dirname, '..') });
 const cors = require('cors');
